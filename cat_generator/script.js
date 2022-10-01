38fb9ebc603c7805dd71c69eddcbf7dd6cd31f9b
Rajpreetkaur02 <78468285+Rajpreetkaur02@users.noreply.github.com>
2022-10-01 23:29:11 +0530

    Revert "added cat generator"

--- cat_generator/script.js
+++
@@ -1,6 +0,0 @@
-function generateCat(){
-    var image = document.createElement('img');
-    var div = document.getElementById('imgGenerator');
-    image.src = "cute-cat-gif.gif";
-    div.appendChild(image);
-}  
